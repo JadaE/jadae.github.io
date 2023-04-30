3e146efe8e04f599c90496d7782e51eb4e3b7834
Jada Ebong <jadaebong@gmail.com>
2023-04-29 19:16:34 -0500

    adding mantine react

---
+++ my-mantine-app/src/App.tsx
@@ -0,0 +1,36 @@
+import { MantineProvider, Text } from '@mantine/core';
+
+export default function App() {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Text>Welcome to Mantine!</Text>
+    </MantineProvider>
+  );
+}
+
+/*import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
