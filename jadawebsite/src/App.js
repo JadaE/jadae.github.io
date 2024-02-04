8117223495ba1cc94ed56b7b535e4ba3e1c87331
Jada Ebong <jadaebong@gmail.com>
2024-02-03 20:13:36 -0600

    more stuff

--- jadawebsite/src/App.js
+++ jadawebsite/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import AboutMe from "./routes/AboutMe";
+import Home from "./routes/Home";
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+      </Routes>
+    </>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
